8cffba43acd4abcbf151a380525092ef1aa7f515
Vedanta Somnathe <vedanta1412@gmail.com>
2022-08-12 23:55:37 +0530

    hogy

--- components/Navs.jsx
+++ components/Navs.jsx
@@ -2,14 +2,63 @@ import React from "react";
 import Link from "next/link";
 import { FaSearch } from "react-icons/fa";
 
-const SubNav = () => {
+const SubNav = ({ setShoes = (the) => console.log(the) }) => {
     const links = ["Men", "Women", "Kids"];
+
+    const change = (e) => {
+        const men = [
+            {
+                img: "/shoes/1.png",
+                name: "Jorand",
+                link: "/products",
+            },
+            {
+                img: "/shoes/2.png",
+                name: "Jorand",
+                link: "/products",
+            },
+            {
+                img: "/shoes/3.png",
+                name: "Jorand",
+                link: "/products",
+            },
+            {
+                img: "/shoes/4.png",
+                name: "Jorand",
+                link: "/products",
+            },
+        ];
+        const women = [
+            {
+                img: "/shoes/5.png",
+                name: "Jorand",
+                link: "/products",
+            },
+            {
+                img: "/shoes/6.png",
+                name: "Jorand",
+                link: "/products",
+            },
+            {
+                img: "/shoes/7.png",
+                name: "Jorand",
+                link: "/products",
+            },
+        ];
+
+        if (e.target.textContent.trim().toLowerCase() === "men") {
+            setShoes(men);
+        } else {
+            setShoes(women);
+        }
+    };
 
     return (
         <nav className="w-screen z-10 cont absolute flex items-center justify-center top-1/4 left-0 bg-white">
             {links.map((link, i) => (
                 <button
                     className="text-xl transition-all hover:font-bold mx-16 py-2 uppercase z-20"
+                    onClick={(eve) => change(eve)}
                     key={i}
                 >
                     {link}
@@ -20,23 +69,21 @@ const SubNav = () => {
 };
 
 const Nav = () => {
-    const links = ["Products", "Contact", "About"];
+    const links = ["Home", "Products", "Contact"];
 
     return (
-        <nav className="cont z-10 fixed top-0 left-0 pt-20 flex items-center">
+        <nav className="cont z-10 absolute top-0 left-0 pt-20 flex items-center">
             <FaSearch className="mr-10 text-xl cursor-pointer" />
             <div className="links flex items-center">
                 {links.map((link, i) => (
-                    <>
-                        <Link key={i} href={`/${link.toLowerCase()}`}>
-                            <a className="link">
-                                {link}
-                                {i !== links.length - 1 && (
-                                    <div className="nav-line absolute right-0 top-1/2 h-2/3 bg-black"></div>
-                                )}
-                            </a>
-                        </Link>
-                    </>
+                    <Link key={i} href={`/${link.toLowerCase()}`}>
+                        <a className="link">
+                            {link}
+                            {i !== links.length - 1 && (
+                                <div className="nav-line absolute right-0 top-1/2 h-2/3 bg-black"></div>
+                            )}
+                        </a>
+                    </Link>
                 ))}
             </div>
         </nav>
--- pages/products.jsx
+++ pages/products.jsx
@@ -1,40 +1,66 @@
+import Image from "next/image";
+import Link from "next/link";
+import { useState, useEffect } from "react";
 import { Nav, SubNav } from "../components/Navs";
+import Tilt from "react-tilt";
 
 const Products = () => {
-    return (
-        <main className="relative min-h-screen">
-            <Nav />
-            <SubNav />
+    const [shoes, setShoes] = useState([]);
 
-            <Cards />
+    useEffect(() => {
+        setShoes([
+            {
+                img: "/shoes/1.png",
+                name: "Jorand",
+                link: "/products",
+            },
+            {
+                img: "/shoes/2.png",
+                name: "Jorand",
+                link: "/products",
+            },
+            {
+                img: "/shoes/3.png",
+                name: "Jorand",
+                link: "/products",
+            },
+            {
+                img: "/shoes/4.png",
+                name: "Jorand",
+                link: "/products",
+            },
+        ]);
+    }, []);
+
+    return (
+        <main className="relative h-screen">
+            <Nav />
+            <SubNav setShoes={setShoes} />
+
+            <div style={{ height: "36%" }}></div>
+            <Cards shoes={shoes} />
         </main>
     );
 };
 
-const Cards = () => {
+const Cards = ({ shoes }) => {
     return (
-        <div className="cards">
-            {[
-                {
-                    img: "/shoe.jpeg",
-                    name: "Jorand",
-                },
-                {
-                    img: "/shoe.jpeg",
-                    name: "Jorand",
-                },
-                {
-                    img: "/shoe.jpeg",
-                    name: "Jorand",
-                },
-                {
-                    img: "/shoe.jpeg",
-                    name: "Jorand",
-                },
-            ].map((shoe, i) => (
-                <div className="card" key={i}>
-                    {shoe.name}
-                </div>
+        <div className="cards cont flex flex-wrap justify-between items-center">
+            {shoes.map((shoe, i) => (
+                <Link href={shoe.link} key={i}>
+                    <Tilt className="card p-10 rounded-xl shadow-lg mx-1 mb-10 flex flex-col items-center transition-all hover:bg-white">
+                        <Image
+                            src={shoe.img}
+                            alt="shoe"
+                            width="500"
+                            height="480"
+                            objectFit="contain"
+                        />
+                        <span className="uppercase font-bold text-6xl text-gray-600 opacity-40 -translate-y-6 tracking-widest">
+                            {shoe.name}
+                        </span>
+                    </Tilt>
+                </Link>
             ))}
         </div>
     );
